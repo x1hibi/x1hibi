1a267e1d6cd5c2753e5a36ff2b541db8eed0e143
x1hibi <kamisama@comunidad.unam.mx>
2023-04-05 11:15:02 -0600

    bar chart update

---
+++ projects/ScatterplotGraph/app.js
@@ -0,0 +1,185 @@
+// Get data from freecodecamp
+let url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
+let graphData = ""
+let tool = document.getElementById("tooltip")
+
+// Make a GET request to get data
+fetch(url).then(response => response.json())
+          .then(data => {
+            graphData = data
+            makeBarChart(graphData)
+            console.log(data)
+        })
+
+/**
+ * Create a bar chart with array of data
+ * @param {Array} data - Data of bar chart
+ */
+function makeBarChart(dataSet) {
+
+
+     
+    // Check if graph exist 
+    if (scatterplot.children.length != 0) {
+        scatterplot.innerHTML=''
+    }
+
+    // Define size of graph using device size
+    // let width = scatterplot.offsetWidth
+    // let height = scatterplot.offsetHeight
+    let width = 1000
+    let height = 1000
+    let padding = 0
+
+    // Create graph container
+    const svg = d3.select("#scatterplot")
+                .append("svg")
+                .attr("width",width)
+                .attr("height",height)
+                .attr("class","scatterplot")
+
+    console.log("max",d3.max(dataSet,d => d.Time))
+    console.log("max",d3.min(dataSet,d => d.Time))
+
+    // Create a x scale
+    const xScale = d3.scaleTime()
+    .domain([Date.parse(d3.min(dataSet,d => d.Time)),Date.parse(d3.max(dataSet,d => d.Time))])
+    .range([padding,width - padding]); 
+
+    // Create a y scale
+    const yScale = d3.scaleTime()
+    .domain([d3.max(dataSet,d => d.Time),d3.min(dataSet,d => d.Time)])
+    .range([padding,height - padding]);
+
+    console.log(new Date("39:50"))
+    //console.log("max scale",yScale(Dat("39:50")))
+
+    // Insert data into graph 
+    svg.selectAll("circle")
+       .data(dataSet)
+       .enter()
+       .append("circle")
+       .attr("class","circle")
+       .attr("cx",(d,i)=> i*2)  
+       .attr("cy",(d,i)=> i )  
+       .attr("r",d=> 5)  
+
+
+
+    /*
+    // Check if graph exist 
+    if (scatterplot.children.length != 0) {
+        scatterplot.innerHTML=''
+    }
+
+    // Define size of graph using device size
+    let width = scatterplot.offsetWidth
+    let height = scatterplot.offsetHeight
+    let padding = 50
+
+    // Create graph container
+    const svg = d3.select("#scatterplot")
+                .append("svg")
+                .attr("width",width)
+                .attr("height",height)
+                .attr("class","scatterplot")
+
+    // Create a x scale
+    const xScale = d3.scaleTime()
+    .domain([Date.parse(d3.min(dataSet,d => d[0])),Date.parse(d3.max(dataSet,d => d[0]))])
+    .range([padding,width - padding]); 
+
+    // Scale for position 
+    const xScale2 = d3.scaleLinear()
+    .domain([0,275])
+    .range([padding,width - padding]);  
+
+    // Create a y scale
+    const yScale = d3.scaleLinear()
+    .domain([d3.max(dataSet,d => d[1]),0])
+    .range([padding,height - padding]);
+
+    // Two ways to set a y scale 
+    //.domain([0,d3.max(dataSet,d => d[1])])
+    //.range([height - padding,padding]);    
+     
+
+    // Create axis
+    const xAxis = d3.axisBottom(xScale).ticks(10);
+    const yAxis = d3.axisLeft(yScale);
+
+    // Width size calculated
+    let barWidth = (width - padding*2)/dataSet.length
+
+    // Insert data into graph 
+    svg.selectAll("rect")
+       .data(dataSet)
+       .enter()
+       .append("rect")
+       .attr("class","bar")
+       .attr("data-date",d => d[0])
+       .attr("data-gdp",d => d[1])
+       .attr("title",d => d[1])
+       .attr("x",(d,i)=> xScale2(i))  
+       .attr("y",d => yScale(d[1]))
+       .attr("width",barWidth)
+       .attr("height", d => height - yScale(d[1]) - padding)
+       .on("mouseover",e=>{
+            let date0 = e.target.getAttribute("data-date")
+            let date1 = e.target.getAttribute("data-gdp")
+            tool.setAttribute("data-date", date0)
+            tool.style.display= "block"
+            tool.children[0].textContent = date0
+            tool.children[2].textContent = date1
+            tool.style.left = `${e.clientX+15}px`
+            tool.style.top = `${e.clientY}px`   
+         })
+         .on("mouseout", ()=>{
+            tool.style.display= "none"
+        })
+
+    svg.append("g")
+    .attr("id","x-axis")
+    .attr("transform", "translate(0," + (height - padding) + ")")
+    .call(xAxis);
+
+
+    // Add y axis
+    svg.append("g")
+    .attr("id","y-axis")
+    .attr("transform", "translate(" + padding + ",0)")
+    .call(yAxis);
+
+    // Add a title
+    svg.append("text")
+        .attr("id","title")
+        .attr("class", "graphTitle")
+        .attr("text-anchor", "middle")
+        .attr("y", padding/3)
+        .attr("x", width/2)
+        .attr("dy", ".75em")
+        .text("United States GDP");
+
+    svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("class", "axisLabel")
+    .attr("x", width - 5)
+    .attr("y", height-padding)
+    .text('Years');  
+    
+    svg.append("text")
+    .attr("class", "axisLabel")
+    .attr("text-anchor", "end")
+    .attr("y", padding/3)
+    .attr("x", padding)
+    .attr("dy", ".75em")
+    .text("GDP");
+
+    */
+
+}
+
+/*** Make a responsive graph ***/
+window.addEventListener('resize', () => { 
+    makeBarChart(graphData)
+});
